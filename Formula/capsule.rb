536abda791253526889c71da7d43b7014128534f
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2026-06-11 18:25:50 -0700

    update brew tap

--- Formula/capsule.rb
+++ Formula/capsule.rb
@@ -1,41 +1,47 @@
 class Capsule < Formula
-  desc "CLI for the Capsule server"
-  homepage "https://github.com/withcapsule/CLI"
-  version "0.1.5"
-  license "MIT"
+	desc "CLI for the Capsule server"
+	homepage "https://github.com/withcapsule/CLI"
+	version "0.1.5"
+	license "MIT"
 
-  # build from source
-  on_macos do
-    depends_on "rust" => :build
+	url "https://github.com/withcapsule/CLI/archive/refs/tags/v#{version}.tar.gz"
+	sha256 "REPLACE_WITH_SHA256"
 
-    url "https://github.com/withcapsule/CLI/archive/refs/tags/v#{version}.tar.gz"
-    sha256 "REPLACE_WITH_SHA256_OF_SOURCE_TARBALL"
-  end
+	depends_on "rust" => :build
 
-  # install precompiled binary
-  on_linux do
-    on_arm do
-      url "https://github.com/withcapsule/CLI/releases/download/v#{version}/capsule-aarch64-linux.tar.gz"
-      sha256 "f4d6713b3f0e245c578a3516c85739e9f2565da43074afef5e1492f23806b974"
-    end
-    on_intel do
-      url "https://github.com/withcapsule/CLI/releases/download/v#{version}/capsule-x86_64-linux.tar.gz"
-      sha256 "35e4f994220b3bdacb6fb3e21be74452e6a611c1907c084319719468a30c15a6"
-    end
-  end
+	on_macos do
+		on_arm do
+			url "https://github.com/withcapsule/CLI/releases/download/v#{version}/capsule-aarch64-macos.tar.gz"
+			sha256 "REPLACE_WITH_SHA256"
+		end
+		on_intel do
+			url "https://github.com/withcapsule/CLI/releases/download/v#{version}/capsule-x86_64-macos.tar.gz"
+			sha256 "REPLACE_WITH_SHA256"
+		end
+	end
 
-  def install
-    if OS.mac?
-      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
-    else
-      bin.install "capsule"
-    end
+	on_linux do
+		on_arm do
+			url "https://github.com/withcapsule/CLI/releases/download/v#{version}/capsule-aarch64-linux.tar.gz"
+			sha256 "f4d6713b3f0e245c578a3516c85739e9f2565da43074afef5e1492f23806b974"
+		end
+		on_intel do
+			url "https://github.com/withcapsule/CLI/releases/download/v#{version}/capsule-x86_64-linux.tar.gz"
+			sha256 "35e4f994220b3bdacb6fb3e21be74452e6a611c1907c084319719468a30c15a6"
+		end
+	end
 
-    generate_completions_from_executable(bin/"capsule", "completions")
-  end
+	def install
+		if File.exist?("Cargo.toml")
+			system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+		else
+			bin.install "capsule"
+		end
 
-  test do
-    system bin/"capsule", "--version"
-  end
+		generate_completions_from_executable(bin/"capsule", "completions")
+	end
+
+	test do
+		system bin/"capsule", "--version"
+	end
 end
-
